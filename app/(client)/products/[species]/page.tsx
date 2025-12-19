f8876dc321194da6f3fdc7fb883cac113783108f
yuyyuyswallowtail <yuyyuyswallowtail@gmail.com>
2025-12-19 17:38:56 +0700

    refactor: Remove fade up animation from detail product

--- app/(client)/products/[species]/page.tsx
+++ app/(client)/products/[species]/page.tsx
@@ -1,7 +1,6 @@
 import AutoBreadcrumb from "@/components/AutoBreadcrumb";
 import { TypographyH1, TypographyP } from "@/components/Typography";
 import { ZoomImage } from "@/components/ZoomImage";
-import FadeUp from "@/components/animation/FadeUp";
 import InfiniteProductStocks from "@/components/product/InfiniteScrollProductStocks";
 import { Product } from "@/components/product/ProductItems";
 import products from "@/datas/product.json"
@@ -17,29 +16,27 @@ const ProductDetail = async ({ params }: { params: Promise<{ species: string }> 
     <AutoBreadcrumb />
 
     <section className="flex flex-col justify-center items-center p-4 text-justify gap-4">
-      <FadeUp>
-        {
-          !species || !filteredProducts ?
-            <div className="flex justify-center items-center min-h-dvh">
-              <TypographyH1>
-                Oops, Data Products Cannot Be Found!
-              </TypographyH1>
-            </div>
-            :
-            <div className="flex flex-col justify-center items-center text-justify max-w-160 w-full gap-4 p-4 capitalize">
-              <TypographyH1>{filteredProducts.species}</TypographyH1>
-              <ZoomImage>
-                <Image src={filteredProducts.path} alt={filteredProducts.species} width={500} height={500} loading="lazy" className="w-full h-auto rounded-md mt-4" />
-              </ZoomImage>
-              <TypographyP className="whitespace-pre-line">{filteredProducts.detail}</TypographyP>
-              {
-                filteredProducts.stocks.length > 1 ?
-                  <InfiniteProductStocks stocks={filteredProducts.stocks} />
-                  : null
-              }
-            </div>
-        }
-      </FadeUp>
+      {
+        !species || !filteredProducts ?
+          <div className="flex justify-center items-center min-h-dvh">
+            <TypographyH1>
+              Oops, Data Products Cannot Be Found!
+            </TypographyH1>
+          </div>
+          :
+          <div className="flex flex-col justify-center items-center text-justify max-w-160 w-full gap-4 p-4 capitalize">
+            <TypographyH1>{filteredProducts.species}</TypographyH1>
+            <ZoomImage>
+              <Image src={filteredProducts.path} alt={filteredProducts.species} width={500} height={500} loading="lazy" className="w-full h-auto rounded-md mt-4" />
+            </ZoomImage>
+            <TypographyP className="whitespace-pre-line">{filteredProducts.detail}</TypographyP>
+            {
+              filteredProducts.stocks.length > 1 ?
+                <InfiniteProductStocks stocks={filteredProducts.stocks} />
+                : null
+            }
+          </div>
+      }
     </section>
   </>
 }
